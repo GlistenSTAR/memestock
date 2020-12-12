52e0a035dee86368f193b8611311fed6b61c2a31
GlistenStar <glistenstar00@gmail.com>
2020-12-12 09:09:33 +0200

    correct datatable initalize

--- index.php
+++ index.php
@@ -176,7 +176,11 @@
 	<!--<script src="assets/js/main.js"></script>-->
 	<script src="https://cdn.datatables.net/1.10.23/js/jquery.dataTables.min.js"></script>
 	<script src="https://cdn.datatables.net/1.10.23/js/dataTables.bootstrap.min.js"></script>
-
+	<script>
+		$(document).ready(function() {
+			$('#example').DataTable();
+		});
+	</script>
 </body>
 
 </html>
