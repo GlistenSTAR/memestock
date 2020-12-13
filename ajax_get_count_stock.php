90b942ff1924d22ccffcf29e33cdfeee432ed7d5
GlistenStar <glistenstar00@gmail.com>
2020-12-13 09:12:38 +0200

    add the stocks and add reddit api

---
+++ ajax_get_count_stock.php
@@ -0,0 +1,10 @@
+<?php
+
+//Uncomment to see the errors
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+require 'vendor/autoload.php';
+require_once('model/TweetStockModel.php');
+
