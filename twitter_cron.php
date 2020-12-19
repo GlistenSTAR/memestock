d0e96726da881bbe88bcd847c2175ad466705749
Glisten **STAR** <75786284+GlistenSTAR@users.noreply.github.com>
2020-12-18 16:20:51 -0800

    try catch inseted

--- twitter_cron.php
+++ twitter_cron.php
@@ -58,7 +58,17 @@ foreach ($dataSliceBatch2 as $key => $value) {
 
   $empty = empty($findStock->tweet_count);
 
-  
+  try {
+    $bodyData = [
+      'stock_keyword' => $stockKeyword
+    ];
+    
+  } catch (\Exception $e) {
+    if ($e->getMessage()) {
+      echo $e->getMessage();
+      continue;
+    }
+  }
 }
 
 }
